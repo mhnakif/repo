1e03174dd7f3bc81e71d3983e6a45db06da468b8
Md Shamim Hussain <snirjhar@gmail.com>
2026-01-17 10:36:27 -0500

    Initial commit

---
+++ launch.sh
@@ -0,0 +1,9 @@
+sudo docker run --rm -it \
+  --gpus all \
+  --ipc=host \
+  --ulimit memlock=-1 \
+  --ulimit stack=67108864 \
+  -p 28188:8188 \
+  -p 28080:8080 \
+  --tmpfs /workspace/scratch:size=200G\
+  nvcr.io/nvidia/pytorch:25.11-py3
